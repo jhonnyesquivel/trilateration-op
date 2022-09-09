50ee21a44f0ddbf6a44845e309d787a5c3f9f8e6
Jhonny Esquivel <jhonnynereos@gmail.com>
2022-09-08 23:34:19 -0500

    added support for env vars for host and db url

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -3,6 +3,8 @@ package bootstrap
 import (
 	"context"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/go-pg/pg/v10"
 	"github.com/jhonnyesquivel/quasar-op/internal/locate"
@@ -10,15 +12,40 @@ import (
 	"github.com/jhonnyesquivel/quasar-op/internal/platform/storage/pgsql"
 )
 
-const (
-	host   = "localhost"
-	port   = 8080
-	dbUser = "postgres"
-	dbPass = "postgrespw"
-	dbHost = "host.docker.internal"
-	dbPort = "49153"
-	dbName = "public"
-)
+type Configuration struct {
+	Host  string
+	Port  uint
+	dbUrl string
+}
+
+var Config Configuration
+
+func init() {
+
+	port, err := strconv.ParseUint(os.Getenv("PORT"), 10, 32)
+	if err != nil {
+		panic("PORT env var has an unvalid value")
+	}
+	Config.Host = os.Getenv("HOST")
+	Config.dbUrl = os.Getenv("DATABASE_URL")
+	Config.Port = uint(port)
+}
+
+func Run() error {
+
+	creds, err := pg.ParseURL(Config.dbUrl)
+	if err != nil {
+		panic("db not ready")
+	}
+
+	db := pg.Connect(creds)
+	db.AddQueryHook(dbLogger{})
+
+	satelliteRepository := pgsql.NewSatelliteRepository(db)
+	topsecretService := locate.NewTopSecretService(satelliteRepository)
+	srv := server.New(Config.Host, Config.Port, topsecretService)
+	return srv.Run()
+}
 
 type dbLogger struct{}
 
@@ -30,28 +57,4 @@ func (d dbLogger) AfterQuery(c context.Context, q *pg.QueryEvent) error {
 	fq, _ := q.FormattedQuery()
 	fmt.Println(string(fq))
 	return nil
-}
-
-func Run() error {
-
-	// mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
-	// db := pg.Connect(&pg.Options{
-	// 	Addr:     fmt.Sprintf("%s:%s", dbHost, dbPort),
-	// 	User:     dbUser,
-	// 	Password: dbPass,
-	// 	Database: dbName,
-	// })
-
-	creds, err := pg.ParseURL("postgres://postgres:postgrespw@host.docker.internal:49153/postgres?sslmode=disable")
-	if err != nil {
-		panic("db not ready")
-	}
-
-	db := pg.Connect(creds)
-	db.AddQueryHook(dbLogger{})
-
-	satelliteRepository := pgsql.NewSatelliteRepository(db)
-	topsecretService := locate.NewTopSecretService(satelliteRepository)
-	srv := server.New(host, port, topsecretService)
-	return srv.Run()
 }
